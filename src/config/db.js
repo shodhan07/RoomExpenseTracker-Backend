01c1cfc0b5fbd2902f585fa3d44af45d5f4d7550
shodhan07 <shodhangowda07@gmail.com>
2025-09-20 10:39:46 +0530

    Fixed connection

--- src/config/db.js
+++ src/config/db.js
@@ -2,20 +2,11 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  multipleStatements: true
-});
+export const pool = mysql.createPool(process.env.MYSQL_URL);
 
-export async function testConnection(){
+export async function testConnection() {
   const conn = await pool.getConnection();
   await conn.ping();
   conn.release();
+  console.log("✅ Database connection successful!");
 }
